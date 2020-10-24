49cc1048fad4ecc78a8c7af8e772b84aa65b1b5b
Robert Gregg <hi@robertgregg.co.uk>
2020-10-24 10:57:53 +0100

    Attempt to push portfolio changes (#5)

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,10 +1,14 @@
 const portfolioItem1      = document.getElementById('p-item-1');
 const portfolioItem1Hover = document.getElementById('p-item-1-hover');
+const portfolioTags1      = document.getElementById('p-tags-1');
+
 
 portfolioItem1.addEventListener('mouseover', () => {
    portfolioItem1Hover.classList.add('height');
+   portfolioTags1.classList.add('height');
 });
 
 portfolioItem1.addEventListener('mouseout', () => {
     portfolioItem1Hover.classList.remove('height');
+    portfolioTags1.classList.remove('height');
  });
