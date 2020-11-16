99d593fa1fb70e7a00527929ef9b42c468896352
Robert Gregg <hi@robertgregg.co.uk>
2020-11-16 13:14:44 +0000

    --Date picker safari

--- src/portfolio/todo-app.js
+++ src/portfolio/todo-app.js
@@ -78,7 +78,7 @@ function addTodo(el){
                     <span class="sub-task-item-input" contenteditable="true" data-text="+ Add a subtask"></span>
                 </div>
                 <div class="extended-todo-item sb-todo-date">
-                    <input type="date" class="date" name="date" />
+                    <input type="date" class="date" name="date" placeholder="dd/mm/yyyy" pattern="(^(((0[1-9]|1[0-9]|2[0-8])[\/](0[1-9]|1[012]))|((29|30|31)[\/](0[13578]|1[02]))|((29|30)[\/](0[4,6,9]|11)))[\/](19|[2-9][0-9])\d\d$)|(^29[\/]02[\/](19|[2-9][0-9])(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)$)" />
                 </div>
                 <ul class="subtasks">
                 </ul>
