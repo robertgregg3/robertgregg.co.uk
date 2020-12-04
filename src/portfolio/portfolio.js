95652f9bb82025aa596981f9bf6c2c2dc5b3bd7d
Robert Gregg <hi@robertgregg.co.uk>
2020-12-04 15:15:14 +0000

    Safari attempt

--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -227,17 +227,14 @@ function addPortfolioCategory() {
             removeClickedClass(); // remove every pfCategory clicked class before adding it
             addClickedClass(); // set a delay so the class is added after the page transition
             triggerPageTransition(); 
-            setTimeout(filterPortfolioItems, 100);             
-        });
-
-        function filterPortfolioItems(){
+            // setTimeout(filterPortfolioItems, 100); 
             const pfFilterItems = document.querySelectorAll('.pf-item');
 
             pfFilterItems.forEach(item => {
               item.classList[!item.classList.contains(pfCategory.innerText.toLowerCase()) ? 'add' : 'remove']('hidden');
-            });   
-        }
-        
+            });               
+        });
+
         function addClickedClass(){
             pfCategory.classList.add('pf-category-clicked');
         }  
